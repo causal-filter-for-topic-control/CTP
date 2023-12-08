29bc73a4c2e22061958d95011eaf0349737de2b5
Low Rank Causal Filters for Topic Controlled Generation <yourmailid+causal.filter.for.topic.control@gmail.com>
2023-12-08 16:53:10 +0530

    updates

--- sync_code.sh
+++ sync_code.sh
@@ -2,6 +2,6 @@
 
 # Add all changes, commit with the provided message, and push
 git add -A
-# Use single quotes and not double quote. Why?
+
 git commit -m "updates"
 git push --set-upstream origin main
