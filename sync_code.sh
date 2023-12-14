313d591e9e176c4572a0f6efd0a391a78d94a9ef
Low Rank Causal Filters for Topic Controlled Generation <yourmailid+causal.filter.for.topic.control@gmail.com>
2023-12-14 11:48:04 +0530

    updates

--- sync_code.sh
+++ sync_code.sh
@@ -8,7 +8,7 @@ export KEY="calorf"
 export GITHUB_USER="causal.filter.for.topic.control"
 export EMAIL_ID="yourmailid+$GITHUB_USER@gmail.com"
 
-git config --local user.name "$NAME"
+git config --local user.name '$NAME'
 git config --local user.email  $EMAIL_ID
 
 # Add the correct key to the github account
